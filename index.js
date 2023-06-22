4e2d74c088a0d8d5e4c273e2bc605df76e19fc7b
Gabriel Fogaça <gabrielfogassa@gmail.com>
2023-06-22 01:12:20 -0300

    FLuxo de autenticação, cadastro e listagem de usuários

--- index.js
+++ index.js
@@ -1,72 +1,58 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
-const db = require('@cyclic.sh/dynamodb')
+const cyclicDB = require('@cyclic.sh/dynamodb')
+const db = cyclicDB(process.env.CYCLIC_DB);
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-// var options = {
-//   dotfiles: 'ignore',
-//   etag: false,
-//   extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-//   index: ['index.html'],
-//   maxAge: '1m',
-//   redirect: false
-// }
-// app.use(express.static('public', options))
-// #############################################################################
+// Adicionar novos usuários
+app.post('/users/:key', async (req, res) => {
+  const key = req.params.key;
 
-// Create or Update an item
-app.post('/:col/:key', async (req, res) => {
-  console.log(req.body)
+  const item = await db.collection('users').get(key);
 
-  const col = req.params.col
+  if(!item){
+    await db.collection('users').set(key, req.body);
+    res.end('ok');
+
+  }else{
+    res.end('Usuário já cadastrado!');
+  }
+});
+
+// Obter informações de um usuário
+app.get('/users/:key', async (req, res) => {
+  const key = req.params.key;
+
+  const item = await db.collection('users').get(key);
+  
+  res.json(item).end();
+})
+
+// Fluxo de autenticação
+app.get('/auth/:key', async (req, res) => {
   const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).set(key, req.body)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
 
-// Delete an item
-app.delete('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} delete key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).delete(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+  const item = await db.collection('auth').get(key);
 
-// Get a single item
-app.get('/:col/:key', async (req, res) => {
-  const col = req.params.col
-  const key = req.params.key
-  console.log(`from collection: ${col} get key: ${key} with params ${JSON.stringify(req.params)}`)
-  const item = await db.collection(col).get(key)
-  console.log(JSON.stringify(item, null, 2))
-  res.json(item).end()
-})
+  if(item.props.password == req.body.password){
+    res.json({authenticated: true});
 
-// Get a full listing
-app.get('/:col', async (req, res) => {
-  const col = req.params.col
-  console.log(`list collection: ${col} with params: ${JSON.stringify(req.params)}`)
-  const items = await db.collection(col).list()
-  console.log(JSON.stringify(items, null, 2))
-  res.json(items).end()
-})
+  }else{
+    res.json({authenticated: false});
+  }
+});
 
 // Catch all handler for all other request.
 app.use('*', (req, res) => {
-  res.json({ msg: 'no route handler found' }).end()
+  res.json({ msg: 'no route handler found' }).end();
 })
 
 // Start the server
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`index.js listening on ${port}`)
+  console.log(`index.js listening on ${port}`);
 })
